67b63c5a6c3bd0b497597ab80a9444783346c2c8
Timothy Huesmann <timothy.huesmann@gmail.com>
2016-04-23 13:41:40 -0500

    put together profiles in Firebase and started grabbing Campaigns from Firebase to display on the admin page

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -2,5 +2,9 @@ var app = angular.module('arayzApp');
 app.controller('adminController', function($scope, $firebaseArray){
 	var ref = new Firebase("http://arayz.firebaseio.com/");
 	var authData = ref.getAuth();
+
+	var campaign_data = ref.child("Campaigns");
+	$scope.campaigns = $firebaseArray(campaign_data);
+
 	
 });
