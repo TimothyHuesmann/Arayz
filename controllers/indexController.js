0d09778f1f5f2b77d81240a187ad21f11834f56e
Timothy Huesmann <timothy.huesmann@gmail.com>
2016-04-23 12:14:42 -0500

    page is functional from an angular perspective

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -5,23 +5,33 @@ app.controller('indexController', function($scope){
 	$scope.isAuthenticated = false;
 	$scope.authData = authData;
 
+	if(authData){
+		$scope.isAuthenticated = true;
+		$scope.userEmail = authData.password.email;
+		ref.child("role").child(authData.uid).on("value", function(data){
+			$scope.role = data.val();
+			$scope.$apply();
+		});
+	}
+
 	$scope.logout = function(){
 		ref.unauth();
 		location.reload();
 	}
 
 	$scope.login = function(){
-		ref.authWithPassword({
-			email : this.username,
-			password : this.password
-		}, function(error, authData){
-			if(error){
-				console.log(error);
-			}
-			else{
-				location.reload();
-			}
-		}
-	});
-	};
+        ref.authWithPassword({
+        email    : this.username,
+        password : this.password
+    }, function(error, authData){
+        if (error) 
+        {
+            console.log(error);
+        }
+        else 
+        {
+            location.reload();        
+        }
+    });
+    };
 });
